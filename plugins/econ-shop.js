191601c57e808cf99f64665e837ff1b05f9ddab3
おDaniel <122085369+DanisOFC@users.noreply.github.com>
2023-06-08 03:34:58 -0600

    ItsukiBot - MD

--- plugins/econ-shop.js
+++ plugins/econ-shop.js
@@ -1,22 +1,20 @@
-//import db from '../lib/database.js'
-
-const xpperdiamond = 350 
+const xpperlimit = 350
 let handler = async (m, { conn, command, args }) => {
   let count = command.replace(/^buy/i, '')
-  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperdiamond) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
+  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
   count = Math.max(1, count)
-  if (global.db.data.users[m.sender].exp >= xpperdiamond * count) {
-    global.db.data.users[m.sender].exp -= xpperdiamond * count
-    global.db.data.users[m.sender].diamond += count
+  if (global.db.data.users[m.sender].exp >= xpperlimit * count) {
+    global.db.data.users[m.sender].exp -= xpperlimit * count
+    global.db.data.users[m.sender].limit += count
     conn.reply(m.chat, `
 ┌─「 *NOTA DE PAGO* 」
-‣ *Compra nominal* : + ${count}💎 
-‣ *Gastado* : -${xpperdiamond * count} XP
+‣ *Compra nominal* : + ${{count}💎 
+‣ *Gastado* : -${xpperlimit * count} XP
 └──────────────`, m)
   } else conn.reply(m.chat, `❎ Lo siento, no tienes suficientes *XP* para comprar *${count}* Diamantes💎\n\n Puedes conseguir *XP* usando los comandos del *menú juegos y economía*`, m)
 }
-handler.help = ['buy', 'buyall']
-handler.tags = ['econ']
+handler.help = ['Buy', 'Buyall']
+handler.tags = ['rpg']
 handler.command = ['buy', 'buyall'] 
 
 handler.disabled = false
