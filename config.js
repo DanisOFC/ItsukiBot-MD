94487a51c8884246bf596a261db7f2bab7c78477
おDaniel <122085369+DanisOFC@users.noreply.github.com>
2023-07-12 11:51:03 -0600

    Add files via upload

--- config.js
+++ config.js
@@ -37,13 +37,6 @@ global.vs = 'ItsukiBot - MD'
 
 //━━━━━━━━━━━ ฅ^•ﻌ•^ฅ ━━━━━━━━━━━
 
-global.openai_key = 'sk-...OzYy'
-/* Consigue tu ApiKey en este enlace: https://platform.openai.com/account/api-keys */
-
-global.openai_org_id = 'HITjoN7H8pCwoncEB9e3fSyW'
-/* Consigue tu ID de organizacion en este enlace: https://platform.openai.com/account/org-settings */
-
-
 global.keysZens = ['LuOlangNgentot', 'c2459db922', '37CC845916', '6fb0eff124', 'hdiiofficial', 'fiktod', 'BF39D349845E', '675e34de8a', '0b917b905e6f']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
