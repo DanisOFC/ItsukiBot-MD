3c004ece2e65f2738a8575e1fbfb0ce5fe7745d2
おDaniel <122085369+DanisOFC@users.noreply.github.com>
2023-07-11 21:27:32 -0600

    Add files via upload

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -7,7 +7,6 @@ let chat = global.db.data.chats[m.chat]
 let bot = global.db.data.settings[this.user.jid] || {}
 if (bot.antiPrivate && !isOwner && !isROwner) {
 await m.reply(`*Holα @${m.sender.split`@`[0]}, no es permitido escribir αl privαdo del bot, αsí que serαs bloqueαdo por ser un gαy que no sαbe usαr el bot en grupo*`, false, { mentions: [m.sender] })
-await m.reply('*Si eres Sub Bot y te ha bloqueado la Bot principal entra a éste grupo:* https://chat.whatsapp.com/C1ujMrunSAq7dkD2UB5H3z')
 await this.updateBlockStatus(m.chat, 'block')}
 return !1
 }
